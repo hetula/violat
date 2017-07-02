e1f4ebb66669ebd34fe80901a69e7559df024ded
Tuomo Heino <tuomo.heino@hotmail.com>
2017-07-02 11:46:20 +0300

    Refactor Main loop
    Refactors Main loop for clarity.

--- src/violat.c
+++ src/violat.c
@@ -49,12 +49,15 @@
 #define NO_SONG -1
 #endif
 
+// Console colors
+#define CLR_BLACK   "\x1b[30m"
 #define CLR_RED     "\x1b[31m"
 #define CLR_GREEN   "\x1b[32m"
 #define CLR_YELLOW  "\x1b[33m"
 #define CLR_BLUE    "\x1b[34m"
 #define CLR_MAGENTA "\x1b[35m"
 #define CLR_CYAN    "\x1b[36m"
+#define CLR_WHITE   "\x1b[37m"
 #define CLR_CLEAR   "\x1b[0m"
 
 #define PLAY_MODE_NORMAL 0
@@ -62,14 +65,23 @@
 #define PLAY_MODE_SINGLE 2
 
 const char *HELP =
-        "Commands:\n"
-                "help - Prinst this help\n"
-                "play - Plays song\n"
-                "stop - Stops playback\n"
-                "next - Plays next song, loops around\n"
-                "prev - Plays previous song, loops around\n"
-                "pause - Pauses playback\n"
-                "resume - Resumes playback";
+        "\x1b[32;1mCommands:\x1b[0m\n"
+                "\x1b[34;1mhelp\x1b[0m   - Prints this help\n"
+                "\x1b[34;1mscan\x1b[0m   - Scan new songs in to library\n"
+                "\x1b[34;1mvol\x1b[0m    - Change volume\n"
+                "\n"
+                "\x1b[34;1mplay\x1b[0m   - Play song\n"
+                "\x1b[34;1mstop\x1b[0m   - Stop playback\n"
+                "\x1b[34;1mpause\x1b[0m  - Pause playback\n"
+                "\x1b[34;1mresume\x1b[0m - Resume playback\n"
+                "\x1b[34;1mnext\x1b[0m   - Play next song\n"
+                "\x1b[34;1mprev\x1b[0m   - Play previous song\n"
+                "\x1b[34;1mnow\x1b[0m    - Show currently playing song\n"
+                "\n"
+                "\x1b[34;1mmode\x1b[0m   - Show current playback mode\n"
+                "\x1b[34;1mnormal\x1b[0m - Change to normal playback mode\n"
+                "\x1b[34;1mrandom\x1b[0m - Change to random playback mode\n"
+                "\x1b[34;1msingle\x1b[0m - Change to single playback mode";
 
 const char *logo =
         " _   _  _         _         ___  ___             _               _                           \n"
@@ -93,8 +105,12 @@ pthread_t gobj_thread;
 guint bus_watch_id;
 char *now_playing = NULL;
 
-void parse_args(int argc, char **argv);
+// Callback functions
+int walk_dir_tree(const char *const dirpath);
 
+gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data);
+
+// Non externed
 void free_pipeline_if_needed();
 
 void play_file(char *play, int print_info);
@@ -105,27 +121,26 @@ void set_next_song();
 
 void set_prev_song();
 
-void pause_playback();
-
-void resume_playback();
-
 void play_last_sel(int print_info);
-
-int add_entry(const char *filepath);
 
 int get_music_files(const char *filepath, const struct stat *info, int typeflag, struct FTW *pathinfo);
 
-int walk_dir_tree(const char *const dirpath);
-
-gboolean bus_callback(GstBus *bus, GstMessage *message, gpointer data);
-
 int read_int();
-
-int select_file();
 
 char *read_in();
 
-void player_interface();
+// Externed
+extern inline void parse_args(int argc, char **argv);
+
+extern inline void pause_playback();
+
+extern inline void resume_playback();
+
+extern inline int add_entry(const char *filepath);
+
+extern inline int select_file();
+
+extern inline void player_interface();
 
 /**
  * Checks if params contain only -v or --version
@@ -133,14 +148,14 @@ void player_interface();
  * @param argv arguments
  * @return if was only -v or --version
  */
-int only_version(int argc, char **argv) {
+extern inline int only_version(int argc, char **argv) {
     return argc == 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0);
 }
 
 /**
  * Print gstreamers linked info
  */
-void print_gstream_info() {
+extern inline void print_gstream_info() {
     const gchar *nano_str;
     guint major, minor, micro, nano;
     gst_version(&major, &minor, &micro, &nano);
@@ -169,7 +184,7 @@ void *gobj_main_loop_run(void *ptr) {
 /**
  * GObject main loop to get bus events to run
  */
-int start_gobj_thread() {
+extern inline int start_gobj_thread() {
     return pthread_create(&gobj_thread, NULL, gobj_main_loop_run, NULL);
 }
 
@@ -216,22 +231,23 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
-void parse_args(int argc, char **argv) {
-    if (argc < 3) return;
-    char *arg = argv[1];
-    if (strcmp(arg, "-l") == 0 || strcmp(arg, "--library") == 0) {
-        printf("Initial library scan!\n");
-        for (int i = 2; i < argc; i++) {
-            char *param = argv[i];
-            if (strlen(param) == 0 || param[0] == '-') {
-                break;
-            }
-            printf("%s\n", param);
-            new_files = 0;
-            if (walk_dir_tree(param)) {
-                fprintf(stderr, "%s.\n", strerror(errno));
-            } else if (new_files > 0) {
-                printf("%d new songs added!\n", new_files);
+extern inline void parse_args(int argc, char **argv) {
+    if (argc >= 3) {
+        char *arg = argv[1];
+        if (strcmp(arg, "-l") == 0 || strcmp(arg, "--library") == 0) {
+            printf("Initial library scan!\n");
+            for (int i = 2; i < argc; i++) {
+                char *param = argv[i];
+                if (strlen(param) == 0 || param[0] == '-') {
+                    break;
+                }
+                printf("%s\n", param);
+                new_files = 0;
+                if (walk_dir_tree(param)) {
+                    fprintf(stderr, "%s.\n", strerror(errno));
+                } else if (new_files > 0) {
+                    printf("%d new songs added!\n", new_files);
+                }
             }
         }
     }
@@ -289,13 +305,13 @@ void set_prev_song() {
     }
 }
 
-void pause_playback() {
+extern inline void pause_playback() {
     if (pipeline != NULL) {
         gst_element_set_state(pipeline, GST_STATE_PAUSED);
     }
 }
 
-void resume_playback() {
+extern inline void resume_playback() {
     if (pipeline != NULL) {
         gst_element_set_state(pipeline, GST_STATE_PLAYING);
     }
@@ -313,7 +329,7 @@ void play_last_sel(int print_info) {
     play_file(play, print_info);
 }
 
-int add_entry(const char *filepath) {
+extern inline int add_entry(const char *filepath) {
     // 0 results are matching so flip
     if (!fnmatch("*.+(mp3|flac|wav|wma|m4u|aac|ogg)", filepath, FNM_EXTMATCH)) {
         char **temp = realloc(song_library, (file_count + 1) * sizeof(char *));
@@ -403,7 +419,7 @@ int read_int() {
     return integer;
 }
 
-int select_file() {
+extern inline int select_file() {
     printf("%sSelect song%s (%s0 - %d%s) >", CLR_YELLOW, CLR_CLEAR, CLR_GREEN, file_count - 1, CLR_CLEAR);
     int song = read_int();
     if (song == NO_SONG) return NO_SONG;
@@ -429,134 +445,157 @@ char *read_in() {
     return NULL;
 }
 
-void player_interface() {
+/**
+ * Checks if given input matches command
+ * Will free input if matches
+ * @param input input
+ * @param cmd command to match
+ * @param read if command was read and no further commands need to be processed
+ * @return read == 0
+ */
+int check_command(char *input, char *cmd) {
+    if (strcmp(input, cmd) == 0) {
+        free(input);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+extern inline void scan_cmd() {
+    printf("Directory to scan >");
+    char *folder = read_in();
+    if (folder != NULL) {
+        printf("Scanning '%s'\n", folder);
+        new_files = 0;
+        if (walk_dir_tree(folder)) {
+            printf("%s%s.%s\n", CLR_RED, strerror(errno), CLR_CLEAR);
+        } else if (new_files > 0) {
+            printf("%d new songs added!\n", new_files);
+        }
+        free(folder);
+    }
+}
+
+extern inline void vol_cmd() {
+    printf("Enter volume (0-100) >");
+    int new_vol = read_int();
+    if (new_vol != -1 && new_vol >= 0 && new_vol <= 100) {
+        volume = new_vol;
+        set_volume();
+    }
+}
+
+extern inline void play_cmd() {
+    if (file_count == 0) {
+        printf("No songs to play!\n");
+    } else {
+        int song = select_file();
+        if (song != NO_SONG) {
+            last_sel = song;
+            char *resolved = 0;
+            char *play = realpath(song_library[song], resolved);
+            play_file(play, TRUE);
+        }
+    }
+}
+
+extern inline void stop_cmd() {
+    free_pipeline_if_needed();
+}
+
+extern inline void next_cmd() {
+    if (file_count == 0) {
+        printf("No songs to play!\n");
+        return;
+    }
+    set_next_song();
+    play_last_sel(TRUE);
+}
+
+extern inline void prev_cmd() {
+    if (file_count == 0) {
+        printf("No songs to play!\n");
+        return;
+    }
+    set_prev_song();
+    play_last_sel(TRUE);
+}
+
+extern inline void now_cmd() {
+    if (file_count == 0 || now_playing == NULL) {
+        printf("No song playing!\n");
+    } else {
+        printf("%sPlaying: %s%s\n", CLR_GREEN, now_playing, CLR_CLEAR);
+    }
+}
+
+void print_mode_cmd() {
+    char *mode;
+    switch (play_mode) {
+        case PLAY_MODE_RANDOM:
+            mode = "Random";
+            break;
+        case PLAY_MODE_SINGLE:
+            mode = "Single";
+            break;
+        case PLAY_MODE_NORMAL:
+        default:
+            mode = "Normal";
+            break;
+    }
+    printf("Playback mode: %s\n", mode);
+}
+
+void chande_mode_cmd(int mode) {
+    play_mode = mode;
+    print_mode_cmd();
+}
+
+/**
+ * Main Interface loop of Viola
+ * Handles asking input and parsing it
+ */
+extern inline void player_interface() {
     while (1) {
         printf("%sViola%s >", CLR_BLUE, CLR_CLEAR);
         char *input = read_in();
         if (input != NULL) {
-            int cmd_read = 1;
-            if (strcmp(input, "EXIT") == 0) {
+            if (check_command(input, "EXIT")) {
                 printf("Bye bye!\n");
-                free(input);
                 return;
             }
-
-            if (strcmp(input, "scan") == 0) {
-                cmd_read = 0;
-                printf("Directory to scan >");
-                char *folder = read_in();
-                if (folder != NULL) {
-                    printf("Scanning '%s'\n", folder);
-                    new_files = 0;
-                    if (walk_dir_tree(folder)) {
-                        printf("%s%s.%s\n", CLR_RED, strerror(errno), CLR_CLEAR);
-                    } else if (new_files > 0) {
-                        printf("%d new songs added!\n", new_files);
-                    }
-                    free(folder);
-                }
-            }
-            if (cmd_read && strcmp(input, "vol") == 0) {
-                cmd_read = 0;
-                printf("Enter volume (0-100) >");
-                int new_vol = read_int();
-                if (new_vol != -1 && new_vol >= 0 && new_vol <= 100) {
-                    volume = new_vol;
-                    set_volume();
-                }
-            }
-            if (cmd_read && strcmp(input, "help") == 0) {
-                cmd_read = 0;
-                printf("%s\n", HELP);
-            }
-            if (cmd_read && strcmp(input, "play") == 0) {
-                cmd_read = 0;
-                if (file_count == 0) {
-                    printf("No songs to play!\n");
-                    continue;
-                }
-                int song = select_file();
-                if (song != NO_SONG) {
-                    last_sel = song;
-                    char *resolved = 0;
-                    char *play = realpath(song_library[song], resolved);
-                    play_file(play, TRUE);
-                }
-            }
-            if (cmd_read && strcmp(input, "stop") == 0) {
-                cmd_read = 0;
-                free_pipeline_if_needed();
-            }
-            if (cmd_read && strcmp(input, "next") == 0) {
-                cmd_read = 0;
-                if (file_count == 0) {
-                    printf("No songs to play!\n");
-                    continue;
-                }
-                set_next_song();
-                play_last_sel(TRUE);
-            }
-            if (cmd_read && strcmp(input, "prev") == 0) {
-                cmd_read = 0;
-                if (file_count == 0) {
-                    printf("No songs to play!\n");
-                    continue;
-                }
-                set_prev_song();
-                play_last_sel(TRUE);
-            }
-            if (cmd_read && strcmp(input, "pause") == 0) {
-                cmd_read = 0;
+            if (check_command(input, "scan")) {
+                scan_cmd();
+            } else if (check_command(input, "vol")) {
+                vol_cmd();
+            } else if (check_command(input, "play")) {
+                play_cmd();
+            } else if (check_command(input, "stop")) {
+                stop_cmd();
+            } else if (check_command(input, "next")) {
+                next_cmd();
+            } else if (check_command(input, "prev")) {
+                prev_cmd();
+            } else if (check_command(input, "pause")) {
                 pause_playback();
-            }
-            if (cmd_read && strcmp(input, "resume") == 0) {
-                cmd_read = 0;
+            } else if (check_command(input, "resume")) {
                 resume_playback();
-            }
-            if (cmd_read && strcmp(input, "now") == 0) {
-                cmd_read = 0;
-                if (file_count == 0 || now_playing == NULL) {
-                    printf("No song playing!\n");
-                    continue;
-                }
-                printf("%sPlaying: %s%s\n", CLR_GREEN, now_playing, CLR_CLEAR);
-            }
-            if(cmd_read && strcmp(input, "normal") == 0) {
-                cmd_read = 0;
-                play_mode = PLAY_MODE_NORMAL;
-                printf("Playback mode set to 'Normal'\n");
-            }
-            if(cmd_read && strcmp(input, "random") == 0) {
-                cmd_read = 0;
-                play_mode = PLAY_MODE_RANDOM;
-                printf("Playback mode set to 'Random'\n");
-            }
-            if(cmd_read && strcmp(input, "single") == 0) {
-                cmd_read = 0;
-                play_mode = PLAY_MODE_SINGLE;
-                printf("Playback mode set to 'Single'\n");
-            }
-            if (cmd_read && strcmp(input, "mode") == 0) {
-                cmd_read = 0;
-                char *mode;
-                switch (play_mode) {
-                    case PLAY_MODE_RANDOM:
-                        mode = "Random";
-                        break;
-                    case PLAY_MODE_SINGLE:
-                        mode = "Single";
-                        break;
-                    case PLAY_MODE_NORMAL:
-                    default:
-                        mode = "Normal";
-                        break;
-                }
-                printf("Playback mode: %s\n", mode);
-            }
-            if (cmd_read) {
+            } else if (check_command(input, "now")) {
+                now_cmd();
+            } else if (check_command(input, "normal")) {
+                chande_mode_cmd(PLAY_MODE_NORMAL);
+            } else if (check_command(input, "random")) {
+                chande_mode_cmd(PLAY_MODE_RANDOM);
+            } else if (check_command(input, "single")) {
+                chande_mode_cmd(PLAY_MODE_SINGLE);
+            } else if (check_command(input, "mode")) {
+                print_mode_cmd();
+            } else if (check_command(input, "help")) {
+                printf("%s\n", HELP);
+            } else {
                 printf("%sUnrecognized command %s'%s%s%s'\n", CLR_RED, CLR_CLEAR, CLR_MAGENTA, input, CLR_CLEAR);
+                free(input);
             }
-            free(input);
         }
     }
 }
